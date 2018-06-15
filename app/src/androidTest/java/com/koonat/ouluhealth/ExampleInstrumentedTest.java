c8b28b044a35e35d1fffdf815e77a2ef20699083
Natig Babayev <natig.nizami@gmail.com>
2018-06-15 16:31:28 +0300

    Assertion fix

--- app/src/androidTest/java/com/koonat/ouluhealth/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/koonat/ouluhealth/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.koonat.ouluhealthhack2018", appContext.getPackageName());
+        assertEquals("com.koonat.ouluhealth", appContext.getPackageName());
     }
 }
