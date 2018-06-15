22701f012803de9ab8e24f1ac3f45728f2905b84
Natig Babayev <natig.nizami@gmail.com>
2018-06-16 01:44:07 +0300

    Some gradle fixes

--- domain/src/main/java/com/koonat/ouluhealth/domain/interactor/GetMatchedSymptomsInteractor.kt
+++ domain/src/main/java/com/koonat/ouluhealth/domain/interactor/GetMatchedSymptomsInteractor.kt
@@ -1,13 +1,27 @@
 package com.koonat.ouluhealth.domain.interactor
 
+import com.koonat.ouluhealth.domain.CallbackWithResult
 import com.koonat.ouluhealth.domain.UseCaseWithParameter
 import com.koonat.ouluhealth.domain.model.MatchedSymptom
 import com.koonat.ouluhealth.domain.repository.PredictionRepository
 
-class GetMatchedSymptomsInteractor(val predictionRepository: PredictionRepository)
+class GetMatchedSymptomsInteractor(private val predictionRepository: PredictionRepository)
     : UseCaseWithParameter<String, List<MatchedSymptom>> {
 
-    override fun execute(query: String,
+    /**
+     * @param parameter query to search
+     * @param callback callback for observing error or success cases
+     */
+    override fun execute(parameter: String,
                          callback: UseCaseWithParameter.Callback<List<MatchedSymptom>>) {
+        predictionRepository.matchSymptoms(parameter, object : CallbackWithResult<List<MatchedSymptom>> {
+            override fun onSuccess(result: List<MatchedSymptom>) {
+                callback.onSuccess(result)
+            }
+
+            override fun onError(exception: Exception) {
+                callback.onError(exception)
+            }
+        })
     }
 }
